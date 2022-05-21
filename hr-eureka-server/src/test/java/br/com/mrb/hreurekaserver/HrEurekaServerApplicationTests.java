4a1bf0b5bff1e13864dd544869774973d58e8ed1
MauroBraga <M@r202150016774>
2022-05-21 09:46:09 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/br/com/mrb/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mrb.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
